ab8fd7fb415bb7886d4cdd2a251da58326b3da2b
YunusBeytut <yunusbeytut0@gmail.com>
2025-08-01 00:55:25 +0300

    Add files via upload

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "@/components/Navbar"
 import { Hero } from "@/components/Hero"
-import { About } from "@/components/about"
+import { About } from "@/components/About"
 import { Projects } from "@/components/Projects"
 import { Contact } from "@/components/Contact"
 import { Footer } from "@/components/Footer"
